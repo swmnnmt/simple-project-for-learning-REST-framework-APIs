728dcdb31e6afed11790c2f9c8ef9648e5512f8d
Alireza <3awwman@gmail.com>
2020-09-07 19:56:15 +0430

    Book added to models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=150)
+    story_name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(default='', upload_to='story_image/', null=True)
+    fav = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.name
