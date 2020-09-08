a1ac5384ad142f4b91446de0c2997c95c2cd7c35
Alireza <3awwman@gmail.com>
2020-09-08 20:29:26 +0430

    UpdateStoryData API added

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from web.views import GetAllData, GetFavData
+from web.views import GetAllData, GetFavData, UpdateStoryData
 
 urlpatterns = [
     path('get-all-data/', GetAllData.as_view()),
     path('get-favorite-data/', GetFavData.as_view()),
+    path('update-story-data/<int:pk>/', UpdateStoryData.as_view()),
 ]
--- web/views.py
+++ web/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from web.models import Book
 from web.serializers import BookModelSerializer
+
 
 class GetAllData(APIView):
     def get(self, request):
         query = Book.objects.all()
         serializers = BookModelSerializer(query, many=True)
-        return Response(serializers.data, status = status.HTTP_200_OK)
+        return Response(serializers.data, status=status.HTTP_200_OK)
 
 
 class GetFavData(APIView):
     def get(self, request):
-        query = Book.objects.filter(favorite = True)
-        serializers = BookModelSerializer(query, many = True)
-        return Response(serializers.data, status = status.HTTP_200_OK)
+        query = Book.objects.filter(favorite=True)
+        serializers = BookModelSerializer(query, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+class UpdateStoryData(APIView):
+    def get(self, request, pk):
+        query = Book.objects.get(pk=pk)
+        serializers = BookModelSerializer(query)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = Book.objects.get(pk=pk)
+        serializers = BookModelSerializer(query, data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
