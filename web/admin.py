137c978ea1003d8dd9f5be46bcc1c4ae6e9b7943
Alireza <3awwman@gmail.com>
2020-09-08 13:34:38 +0430

    some improvments in admin panel. e.g. displaying data and search box

--- web/admin.py
+++ web/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from web.models import Book
 
-# Register your models here.
-admin.site.register(Book)
+
+class AdminMode(admin.ModelAdmin):
+    list_display = ['story_name', 'author', 'favorite']
+    search_fields = ['story_name', 'author']
+
+
+admin.site.register(Book, AdminMode)
