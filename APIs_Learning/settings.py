d35a510787481698cc02f96ad4491374b0769ba1
Alireza <3awwman@gmail.com>
2020-09-14 22:30:41 +0430

    Authentication API using dj_rest_auth and model added

--- APIs_Learning/settings.py
+++ APIs_Learning/settings.py
@@ -41,7 +41,14 @@ INSTALLED_APPS = [
     # Myapp
     'web',
     'rest_framework',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- APIs_Learning/urls.py
+++ APIs_Learning/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/v1/', include('web.urls')),
 ]
 if settings.DEBUG:
